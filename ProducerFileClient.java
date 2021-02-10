dbed59e5411164d2f993d7a8a655228422fb6f8a
The Learner <samarkant1@yahoo.com>
2021-02-10 11:35:38 +0530

    Update ProducerFileClient.java

--- ProducerFileClient.java
+++ ProducerFileClient.java
@@ -1,60 +1,57 @@
-import org.apache.hadoop.yarn.webapp.hamlet2.Hamlet;
+package producer;
+
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ProducerFileClient {
-    // create configuration
-    private Properties createProducerConfig(){
-        Properties props = new Properties();
-        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.setProperty(ProducerConfig.ACKS_CONFIG, "all");
-        return  props;
+    public static void main(String[] args) throws IOException {
+        ProducerFileClient producerFileClient = new ProducerFileClient();
+        producerFileClient.produceRecordsFromFileToTopic(args[0], args[1], args[2]);
     }
-
-    // create a producer
-    private KafkaProducer createProducer(){
-        Properties props = this.createProducerConfig();
-        KafkaProducer<String,String > producer = new KafkaProducer<String, String>(props);
+    // create configs
+    private Properties createProducerConfigs(String bservers) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bservers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return props;
+    }
+    // create producer
+    private KafkaProducer<String, String> createProducerFromConfigs(String bservers){
+        Properties props = createProducerConfigs(bservers);
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
         return producer;
     }
-
-    private void runProducer(String topic, String filePath) throws IOException {
-        KafkaProducer<String, String> producer = createProducer();
-        FileReader fr = new FileReader(filePath);
+    // read from file and write to topic
+    private void produceRecordsFromFileToTopic(String bservers, String fileLoc, String topic) throws IOException {
+        KafkaProducer<String, String> producer = createProducerFromConfigs(bservers);
+        FileReader fr = new FileReader(fileLoc);
         BufferedReader br = new BufferedReader(fr);
         br.readLine();
-        Long t1 = System.currentTimeMillis();
-        String line;
-        while((line = br.readLine()) != null){
-            ProducerRecord record = new ProducerRecord(topic, line);
-            producer.send(record, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    if(e != null){
-                        System.out.println("Exception happned");
-                        e.printStackTrace();
-                    } else {
-                        System.out.println("The record was produced successfully to Kafka");
+        String line = "";
+        try{
+            while((line = br.readLine()) != null){
+                ProducerRecord<String, String> record = new ProducerRecord<>(topic, line);
+                producer.send(record, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        String recordDetails = "Producer record sent to topic: " + metadata.topic() +
+                                "\nto partition: " + metadata.partition() +
+                                "\nwith offset: " + metadata.offset();
+                        System.out.println(recordDetails);
                     }
-                }
-            });
+                });
+            }
+        } catch (Exception ex){
+            System.out.println("Something gone wrong");
+            ex.printStackTrace();
+        } finally {
+            producer.close();
         }
-        producer.close();
-        Long t2 = System.currentTimeMillis();
-        System.out.println("Time taken to read the file and send to the topic in ms: "+
-                (t2 -t1));
-    }
-
-    public static void main(String[] args) throws IOException {
-        ProducerFileClient pFC = new ProducerFileClient();
-        pFC.runProducer(args[0], args[1]);
     }
 }
